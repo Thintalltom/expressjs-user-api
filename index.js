aa78b155ed5da704cfa66092a4b21163a4a26792
tomide <tomideadeyanju@gmail.com>
2022-12-11 14:31:09 -0800

    change

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const app = express();
 //this app.use state what functions are to be used in the express framework
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-app.use('/api/user', require('./routes/api/user'))
-
+// function the access the users api
+//while using the postman  you must state the /user
+app.use('/user', require('./Api/user'))
 // app.listen is to state the port to be used
 app.listen(4000, () => {
 
